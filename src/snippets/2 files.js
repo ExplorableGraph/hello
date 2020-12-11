fc67151f0dc7fbd860cfef022f0fd594e8a7d15e
Jan Miksovsky <jan@miksovsky.com>
2020-12-11 13:15:57 -0800

    Demos

--- src/snippets/2 files.js
+++ src/snippets/2 files.js
@@ -1,3 +1,6 @@
+import { Files } from "@explorablegraph/node";
 import path from "path";
 import { fileURLToPath } from "url";
 const dirname = path.dirname(fileURLToPath(import.meta.url));
+
+new Files(path.join(dirname, "static"));
