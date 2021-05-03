1ff5a0e952bb86b70e4e38896133ac37c562bb09
Jan Miksovsky <jan@miksovsky.com>
2021-05-03 16:46:01 -0700

    Get hello running again.

--- eg.config.js
+++
@@ -1,9 +0,0 @@
-import { Compose } from "@explorablegraph/core";
-import { builtins } from "@explorablegraph/eg";
-
-function toUpperCase(text) {
-  return text.toUpperCase();
-}
-toUpperCase.usage = `toUpperCase(text)\tTransforms text to uppercase`;
-
-export default new Compose({ toUpperCase }, builtins);
