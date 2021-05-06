ca441b8d1844311f61dd10c168bfc47de9dd4813
Jan Miksovsky <jan@miksovsky.com>
2021-05-05 17:36:04 -0700

    Track changes in ExplorableApp.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-import { Files } from "@explorablegraph/explorable/packages/node/exports.js";
 import { ExplorableApp } from "@explorablegraph/explorable/packages/web/exports.js";
 import { requestListener } from "@explorablegraph/explorable/packages/webserver/exports.js";
 import http from "http";
@@ -8,9 +7,8 @@ import { fileURLToPath } from "url";
 const port = 5000;
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
-const appDirname = path.join(dirname, "src/app/products");
-const files = new Files(appDirname);
-const graph = new ExplorableApp(files);
+const appFolder = path.join(dirname, "src/app");
+const graph = new ExplorableApp(appFolder);
 
 http.createServer(requestListener(graph)).listen(port);
 console.log(`Server running at http://localhost:${port}`);
