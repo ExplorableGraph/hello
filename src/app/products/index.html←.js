34b9d3838ba9135a402e89892b7b9a54819e7255
Jan Miksovsky <jan@miksovsky.com>
2021-05-10 16:08:54 -0700

    Use image path instead of data URL.

--- src/app/products/index.html←.js
+++ src/app/products/index.html←.js
@@ -4,10 +4,7 @@ export default async function () {
     if (key !== "index.html" && !key.startsWith(":")) {
       const title = key[0].toUpperCase() + key.slice(1);
       const photo = await this.get(key, "photo.jpg");
-      const photoString = photo?.toString("base64");
-      const img = photoString
-        ? `<img width="300px" src="data:image/jpg;base64,${photoString}">`
-        : "";
+      const img = photo ? `<img width="300px" src="${key}/photo.jpg">` : "";
       const link = `<p><a href="${key}/">${img}<br>${title}</a></p>`;
       html += link;
     }
