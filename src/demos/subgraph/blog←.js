15ca146cbb5161389a2f4ce7896319dd388ef4c5
Jan Miksovsky <jan@miksovsky.com>
2021-05-18 17:14:13 -0700

    Track changes in ExplorableObject.

--- src/demos/subgraph/blog←.js
+++ src/demos/subgraph/blog←.js
@@ -1,6 +1,6 @@
-import { ExplorableGraph } from "@explorablegraph/explorable";
+import { ExplorableObject } from "@explorablegraph/explorable";
 
-export default new ExplorableGraph({
+export default new ExplorableObject({
   post1: "This is the first post.",
   post2: "This is the second post.",
   post3: "This is the third post.",
