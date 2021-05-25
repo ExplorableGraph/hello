11687066e38e8b52664f1b5b44d9bffe4a9b65f0
Jan Miksovsky <jan@miksovsky.com>
2021-05-25 16:27:25 -0700

    Track changes in explorable.

--- src/demos/transform graph/step1.json←.js
+++ src/demos/transform graph/step1.json←.js
@@ -1,6 +1,6 @@
-import { ExplorableObject } from "@explorablegraph/explorable";
+import { ExplorableGraph, ExplorableObject } from "@explorablegraph/explorable";
 
 export default async function () {
   const data = new ExplorableObject(await this.get("data.json"));
-  return await data.mapValues((value) => value.toUpperCase());
+  return await ExplorableGraph.mapValues(data, (value) => value.toUpperCase());
 }
--- src/demos/transform graph/step2.json←.js
+++ src/demos/transform graph/step2.json←.js
@@ -1,6 +1,6 @@
-import { ExplorableObject } from "@explorablegraph/explorable";
+import { ExplorableGraph, ExplorableObject } from "@explorablegraph/explorable";
 
 export default async function () {
   const data = new ExplorableObject(await this.get("step1.json"));
-  return await data.mapValues((value) => `Hello, ${value}.`);
+  return await ExplorableGraph.mapValues(data, (value) => `Hello, ${value}.`);
 }
