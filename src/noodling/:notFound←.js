1f873bd47edcf74ef69a7b101a2caff95bc9910f
Jan Miksovsky <jan@miksovsky.com>
2021-06-23 16:56:08 -0700

    Move noodling to sandbox repo

--- src/noodling/:notFound←.js
+++
@@ -1,9 +0,0 @@
-export default function (...keys) {
-  const path = [this.params?.notFound, ...keys];
-  return `
-    <h1>Not found</h1>
-    <p>
-      The page ${path.join("/")} could not be found.
-    </p>
-  `;
-}
--- src/noodling/syntax/step1.json←transform(data.json)
+++
@@ -1,4 +0,0 @@
-export default async function () {
-  const data = await this.get("data.json");
-  return `"${data.toUpperCase()}"`;
-}
