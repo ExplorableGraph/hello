504a5e50a923bae6a755a0f4d4c99f02b355de18
Jan Miksovsky <jan@miksovsky.com>
2021-10-01 15:50:41 -0700

    Debugging

--- server.js
+++
@@ -1,8 +0,0 @@
-import { requestListener } from "@explorablegraph/explorable";
-import http from "http";
-import graph from "./graph.js";
-
-const port = 5000;
-
-http.createServer(requestListener(graph)).listen(port);
-console.log(`Server running at http://localhost:${port}`);
