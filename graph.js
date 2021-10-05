c37c72768021771d80577747e85741e590fc6f72
Jan Miksovsky <jan@miksovsky.com>
2021-10-04 17:09:54 -0700

    Demo

--- graph.js
+++
@@ -1,9 +0,0 @@
-import { ExplorableApp } from "@explorablegraph/explorable";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-const appFolder = path.join(dirname, "src/demos");
-const graph = new ExplorableApp(appFolder);
-
-export default graph;
