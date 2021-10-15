d6aac23cee1351ff0d9df7eaf804bb3137a90235
Jan Miksovsky <jan@miksovsky.com>
2021-10-14 20:20:22 -0700

    Demo

--- demos/transformGraph/step1.json=.js
+++
@@ -1,6 +0,0 @@
-import { ExplorableGraph, ExplorableObject } from "@explorablegraph/explorable";
-
-export default async function () {
-  const data = new ExplorableObject(await this.get("data.json"));
-  return await ExplorableGraph.mapValues(data, (value) => value.toUpperCase());
-}
--- demos/transformGraph/step2.json=.js
+++
@@ -1,6 +0,0 @@
-import { ExplorableGraph, ExplorableObject } from "@explorablegraph/explorable";
-
-export default async function () {
-  const data = new ExplorableObject(await this.get("step1.json"));
-  return await ExplorableGraph.mapValues(data, (value) => `Hello, ${value}.`);
-}
