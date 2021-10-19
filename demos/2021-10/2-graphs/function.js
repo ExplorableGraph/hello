a507d6829477598fda48d0da931a8339def00ccf
Jan Miksovsky <jan@miksovsky.com>
2021-10-19 11:51:35 -0700

    Demo

--- demos/2021-10/2-graphs/function.js
+++ demos/2021-10/2-graphs/function.js
@@ -2,14 +2,14 @@ function fn(arg) {
   return `Hello, ${arg}.`;
 }
 
-const keys = ["Alice", "Bob", "Carol"];
+const domain = ["Alice", "Bob", "Carol"];
 
 // ExplorableGraph representation of the function above
 export default {
   // Return the keys in this graph
   // => ["Alice", "Bob", "Carol"]
   async *[Symbol.asyncIterator]() {
-    yield* keys;
+    yield* domain;
   },
 
   // Get the value of a given key
