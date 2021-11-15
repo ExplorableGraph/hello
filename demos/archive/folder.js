b39778b34f528f54cc7fc8d814e7b2f970edc53b
Jan Miksovsky <jan@miksovsky.com>
2021-11-15 09:54:48 -0800

    Demo

--- demos/archive/folder.js
+++ demos/archive/folder.js
@@ -7,11 +7,18 @@ const moduleFolder = path.dirname(fileURLToPath(import.meta.url));
 const filesFolder = path.resolve(moduleFolder, "../people");
 
 // Read the list of files in that folder.
-const fileNames = await fs.readdir(filesFolder);
+const files = await fs.readdir(filesFolder);
+
+// For each file, read the file's contents.
+const buffers = await Promise.all(
+  files.map(async (fileName) => {
+    const filePath = path.join(filesFolder, fileName);
+    const buffer = await fs.readFile(filePath);
+    return buffer;
+  })
+);
 
 // Display the contents of all the files.
-for (const fileName of fileNames) {
-  const filePath = path.join(filesFolder, fileName);
-  const buffer = await fs.readFile(filePath);
+for (const buffer of buffers) {
   console.log(buffer.toString());
 }
