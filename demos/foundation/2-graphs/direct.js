1adbb3ec48f4077c98c88d5dcd6f84c3b75e9a83
Jan Miksovsky <jan@miksovsky.com>
2021-11-15 09:19:09 -0800

    Demo

---
+++ demos/foundation/2-graphs/direct.js
@@ -0,0 +1,14 @@
+// Direct ExplorableGraph representation of a function and domain
+export default {
+  // Return the keys in this graph
+  // => ["Alice", "Bob", "Carol"]
+  async *[Symbol.asyncIterator]() {
+    yield* ["Alice", "Bob", "Carol"];
+  },
+
+  // Get the value of a given key
+  // get("Alice") => "Hello, Alice."
+  async get(key) {
+    return `Hello, ${key}.`;
+  },
+};
